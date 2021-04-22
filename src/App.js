3d50e6174b17eb3bf55ff0cddc9463da3a9e2e54
Andrej Ilievski <mrandrej.ilievski@gmail.com>
2021-04-22 11:37:47 +0200

    Initial commit. It works but it has 0 css, so it's as aesthetic as crumpled paper

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { ApolloClient, InMemoryCache, useQuery, gql } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client/react';
+
+const client = new ApolloClient({ 
+  uri: 'https://api.spacex.land/graphql/',
+  cache: new InMemoryCache()
+});
+
+const FIVE_FLIGHTS = gql`
+{
+  launches(limit: 5) {
+    launch_date_utc
+    launch_success
+    rocket {
+      rocket_name
+    }
+    links {
+      video_link
+    }
+    details
+  }
+}
+`;
+
+function FiveFlights() {
+  const {loading, error, data} = useQuery(FIVE_FLIGHTS);
+
+  if (loading) return <p>Loading ...</p>;
+  if (error) return <p>Something  went wrong. Error: </p>;
+
+  return data.launches.map(({launch_date_utc, launch_success,rocket, links, details}) => (
+    <div key={rocket.rocket_name}>
+      <h2>
+        {rocket.rocket_name}
+      </h2>
+      <h3>
+        Launch date: {launch_date_utc} ({(launch_success) ? "Success": "Failure" })
+      </h3>
+      <p>
+        <bold>Watch it on <a href={links.video_link}>YouTube</a>!</bold>
+      </p>
+      <p>
+        {(details != null) ? details : "No further details available."}
+      </p>
+    </div>
+  ));
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ApolloProvider client={client}>
+    <div>
+      <h2>SpaceX - Five Flights 🚀</h2>
+      <FiveFlights />
     </div>
+    </ApolloProvider>
   );
 }
 
